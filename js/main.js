d2ac8cd550a6e91e4554dd89f820e53cfcfe6cb0
Ryan Flaherty <rcflaherty@gmail.com>
2015-04-25 14:19:26 -0700

    adjusted breakpoints, manipulated jQuery elements.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
-$(document).ready(function() {
-  //ingredients checklist line out
-  $('.checklist').click(function(){
-    $(this).toggleClass('checked');
-    if ($(this).children('span').is('.box')) {
-      $(this).children("span").removeClass('box').addClass('cbox');
-    } else {
-      $(this).children("span").removeClass('cbox').addClass('box');
-    };
-  });
-  //sharebutton links
-  $(".sharebutton").mouseenter(function() {
-      $(".share-menu").fadeIn(1000);
-  });
-  $(".share").mouseleave(function() {
-      $(".share-menu").fadeOut(1000);
-  });
+
+//ingredients checklist line out
+$('.checklist').click(function(){
+  $(this).toggleClass('checked');
+  if ($(this).children('span').is('.box')) {
+    $(this).children("span").removeClass('box').addClass('cbox');
+  } else {
+    $(this).children("span").removeClass('cbox').addClass('box');
+  };
+});
+
+//sharebutton links
+$('.share').mouseenter(function() {
+  $('.share-menu').fadeIn(1000);
+  $('#share').hide();
+});
+$('.share').mouseleave(function() {
+  $('#share').show();
+  $('.share-menu').hide();
 });
